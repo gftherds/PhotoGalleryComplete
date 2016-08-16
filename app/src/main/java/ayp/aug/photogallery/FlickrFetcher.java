f13a7bd5fac4d951e77c8a0485bae1fc6de8af50
Rawin Ngamloet <rawin.n@augmentis.biz>
2016-08-16 17:47:01 +0700

    Fetcher completed

--- app/src/main/java/ayp/aug/photogallery/FlickrFetcher.java
+++ app/src/main/java/ayp/aug/photogallery/FlickrFetcher.java
@@ -1,15 +1,26 @@
 package ayp.aug.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadFactory;
 
 /**
  * Created by wind on 8/16/2016 AD.
  */
 public class FlickrFetcher {
+    private static final String TAG = "FlickrFetcher";
 
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
@@ -44,5 +55,81 @@ public class FlickrFetcher {
 
     public String getUrlString(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    //
+    private static final String FLICKR_URL = "https://api.flickr.com/services/rest/";
+
+    private static final String API_KEY = "9d495812808b84fda6313aa89484f395";
+
+    public String fetchItems() throws IOException {
+        String jsonString = null;
+        String url = Uri.parse(FLICKR_URL).buildUpon()
+                .appendQueryParameter("method", "flickr.photos.getRecent")
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
+                .appendQueryParameter("extras", "url_s")
+                .build().toString();
+
+        jsonString = getUrlString(url);
+
+        Log.i(TAG, "Received JSON: " + jsonString);
+        return jsonString;
+    }
+
+    public void fetchItems(List<GalleryItem> items) {
+        fetchItems(items, null);
+    }
+
+    public void fetchItems(List<GalleryItem> items, CallBack callBack) {
+        try {
+            String jsonStr = fetchItems();
+            if(jsonStr != null) {
+                parseJSON(items, jsonStr, callBack);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "Failed to fetch items", e);
+        }
+    }
+
+    private void parseJSON(List<GalleryItem> newGalleryItemList, String jsonBodyStr, CallBack callBack)
+            throws IOException, JSONException {
+
+        JSONObject jsonBody = new JSONObject(jsonBodyStr);
+        JSONObject photosJson = jsonBody.getJSONObject("photos");
+        JSONArray photoListJson = photosJson.getJSONArray("photo");
+
+        int len = photoListJson.length();
+
+        if(callBack != null)
+            callBack.totalLen(len);
+
+        for(int i = 0; i < photoListJson.length(); i++) {
+
+            JSONObject jsonPhotoItem = photoListJson.getJSONObject(i);
+
+            GalleryItem item = new GalleryItem();
+
+            item.setId(jsonPhotoItem.getString("id"));
+            item.setTitle(jsonPhotoItem.getString("title"));
+
+            if(!jsonPhotoItem.has("url_s")) {
+                continue;
+            }
+
+            item.setUrl(jsonPhotoItem.getString("url_s"));
+            newGalleryItemList.add(item);
+
+            if(callBack != null)
+                callBack.fetchItem(i + 1);
+        }
+    }
+
+    public interface CallBack {
+        void fetchItem(int itemCount);
+        void totalLen(int len);
     }
 }
--- app/src/main/java/ayp/aug/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/ayp/aug/photogallery/PhotoGalleryFragment.java
@@ -1,13 +1,27 @@
 package ayp.aug.photogallery;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wind on 8/16/2016 AD.
@@ -26,6 +40,34 @@ public class PhotoGalleryFragment extends Fragment {
     }
 
     private RecyclerView mRecyclerView;
+    private FlickrFetcher mFlickrFetcher;
+    private List<GalleryItem> mItems;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mnu_reload:
+                new FetcherTask().execute();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Nullable
     @Override
@@ -33,8 +75,99 @@ public class PhotoGalleryFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
 
         mRecyclerView = (RecyclerView) v.findViewById(R.id.photo_gallery_recycler_view);
-        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        Resources r = getResources();
+        int gridSize = r.getInteger(R.integer.grid_size);
 
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), gridSize));
+        mItems = new ArrayList<>();
+        mRecyclerView.setAdapter(new PhotoGalleryAdapter(mItems));
+
+        mFlickrFetcher = new FlickrFetcher();
+        new FetcherTask().execute(); // run another thread
         return v;
+    }
+
+    class PhotoGalleryVH extends RecyclerView.ViewHolder {
+
+        TextView mText;
+
+        public PhotoGalleryVH(View itemView) {
+            super(itemView);
+
+            mText = (TextView) itemView;
+        }
+
+        public void bindGalleryItem(GalleryItem galleryItem) {
+            mText.setText(galleryItem.getTitle());
+        }
+    }
+
+    class PhotoGalleryAdapter extends RecyclerView.Adapter<PhotoGalleryVH> {
+
+        List<GalleryItem> mGalleryItemList;
+
+        PhotoGalleryAdapter(List<GalleryItem> galleryItems) {
+            mGalleryItemList = galleryItems;
+        }
+
+        @Override
+        public PhotoGalleryVH onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(getActivity()).inflate(
+                    android.R.layout.simple_list_item_1, parent, false);
+
+            return new PhotoGalleryVH(v);
+        }
+
+        @Override
+        public void onBindViewHolder(PhotoGalleryVH holder, int position) {
+            holder.bindGalleryItem(mGalleryItemList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mGalleryItemList.size();
+        }
+    }
+
+    class FetcherTask extends AsyncTask<Void, Integer, List<GalleryItem>> implements FlickrFetcher.CallBack {
+
+        int total;
+
+        @Override
+        protected List<GalleryItem> doInBackground(Void... voids) {
+            Log.d(TAG, "Start fetcher task");
+            List<GalleryItem> itemList = new ArrayList<>();
+
+            mFlickrFetcher.fetchItems(itemList, this);
+            Log.d(TAG, "Fetcher task finished");
+            return itemList;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+            String formatString = getResources().getString(R.string.photo_progress_loaded, values[0]);
+            Snackbar.make(mRecyclerView, formatString, Snackbar.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> galleryItems) {
+            mItems = galleryItems;
+            mRecyclerView.setAdapter(new PhotoGalleryAdapter(mItems));
+        }
+
+        ////////////////////////
+
+        @Override
+        public void fetchItem(int itemCount) {
+            float result = (float) itemCount / total * 100;
+            publishProgress( Math.round(result) );
+        }
+
+        @Override
+        public void totalLen(int len) {
+            total = len;
+        }
     }
 }
